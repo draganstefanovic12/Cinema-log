dc09a4543379fe3965477af29ab34302b6949698
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-07-29 20:25:35 +0200

    small css changes

--- src/hooks/useWatchlist.tsx
+++ src/hooks/useWatchlist.tsx
@@ -4,12 +4,16 @@ export const useWatchlist = () => {
   const handleWatchlist = async (
     nm: string,
     poster: string,
-    username: string
+    username: string,
+    id: number,
+    type: string
   ) => {
     await axios.post(`http://localhost:5000/user/watchlist/`, {
       method: "POST",
       body: {
         name: nm,
+        id: id,
+        type: type,
         poster: `https://image.tmdb.org/t/p/w500/${poster}`,
         username: username,
       },
