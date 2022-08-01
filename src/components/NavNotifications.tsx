66902ffd50dfa676f630016803f28263b1a2d7e0
kyogan12 <kyoyoyo122@gmail.com>
2022-08-01 15:59:29 +0200

    made a popper component to hug nav dropdown lists

---
+++ src/components/NavNotifications.tsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { NavPopper } from "./NavPopper";
+import { ListItem, MenuList } from "@mui/material";
+import { formatDistanceToNow } from "date-fns";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import { usePopper } from "../hooks/usePopper";
+
+interface NotificationProps {
+  notifications: [];
+}
+
+interface Notification {
+  content: string | undefined;
+  createdAt: string | number | Date;
+  user: string | undefined;
+  read: boolean;
+}
+
+export const NavNotifications = ({ notifications }: NotificationProps) => {
+  const { setOpen, open } = usePopper();
+
+  return (
+    <NavPopper
+      setOpen={setOpen}
+      open={open}
+      button={<NotificationsNoneOutlinedIcon />}
+    >
+      <MenuList
+        className="menu-list"
+        sx={{
+          color: "rgb(102, 125, 147)",
+          backgroundColor: "#141c30",
+        }}
+      >
+        {notifications.length > 0 ? (
+          notifications.map((notification: Notification) => (
+            <ListItem style={{ whiteSpace: "nowrap" }}>
+              <Link
+                style={{ marginRight: "0.2em" }}
+                to={`/user/${notification.user}`}
+                onClick={() => setOpen(false)}
+              >
+                {notification.user}{" "}
+              </Link>{" "}
+              {notification.content}{" "}
+              <span style={{ marginLeft: "0.5em" }}>
+                {formatDistanceToNow(new Date(notification.createdAt))} ago
+              </span>
+            </ListItem>
+          ))
+        ) : (
+          <ListItem style={{ color: "white" }}>
+            No new notifications...
+          </ListItem>
+        )}
+      </MenuList>
+    </NavPopper>
+  );
+};
