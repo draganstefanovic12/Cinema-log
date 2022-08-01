24e9bd213ebe067706ce263f14f287c36488053d
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-08-01 15:13:18 +0200

    transfered some styles from mui to css file

--- src/components/Genre.tsx
+++ src/components/Genre.tsx
@@ -13,27 +13,9 @@ interface GenreProps {
 
 export const Genre = ({ data }: GenreProps) => {
   return (
-    <Typography
-      sx={{
-        marginTop: "0.5em",
-        display: "flex",
-        gap: "1em",
-        width: "7.3em",
-        height: "2em",
-        whiteSpace: "nowrap",
-      }}
-    >
+    <Typography className="genres">
       {data.genres.map((genre: GenreMapped) => (
-        <ListItem
-          key={genre.id}
-          sx={{
-            marginBottom: "0.2em",
-            paddingBottom: "0.5em",
-            backgroundColor: "#0f172a",
-            justifyContent: "center",
-          }}
-          className="genre"
-        >
+        <ListItem className="genre" key={genre.id}>
           {genre.name}
         </ListItem>
       ))}
