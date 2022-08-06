fe85656dc444da91a33073cb9fb4e427afcf7326
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-08-06 03:42:33 +0200

    rdy to work on trending

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,9 @@
 import { HelmetTitle } from "../components/HelmetTitle";
+import { useFetch } from "../hooks/useFetch";
 
 export const Homepage = () => {
+  const data = useFetch("http://localhost:5000/imdb/trending");
+  data && console.log(data);
   return (
     <section>
       <HelmetTitle title={"Home"} />
