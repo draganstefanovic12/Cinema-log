132b304025b9109a3fa7918898b2e93e37fc3e58
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-08-09 18:34:17 +0200

    image upload feature complete

--- src/components/ImageUploadForm.tsx
+++ src/components/ImageUploadForm.tsx
@@ -1,6 +1,7 @@
 import { Input, MenuList } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { NavPopper } from "./NavPopper";
 
@@ -12,6 +13,7 @@ type Curr = {
 export const ImageUploadForm = ({ current, setImgSrc }: Curr) => {
   const [open, setOpen] = useState<boolean>(false);
   const { userStats } = useAuth();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     document.getElementById("hidden")?.click();
@@ -20,6 +22,9 @@ export const ImageUploadForm = ({ current, setImgSrc }: Curr) => {
   const handleSubmit = () => {
     setOpen(false);
     document.getElementById("submit")?.click();
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
   };
 
   const handleDelete = () => {
@@ -38,11 +43,16 @@ export const ImageUploadForm = ({ current, setImgSrc }: Curr) => {
         action={`http://localhost:5000/image/upload/${userStats?.data.user._id}`}
         encType="multipart/form-data"
       >
-        <MenuList sx={{ padding: "0.5em" }} onClick={handleClick}>
+        <MenuList
+          className="img-form-menu"
+          sx={{ padding: "0.5em" }}
+          onClick={handleClick}
+        >
           Upload file
-          <Input
+          <input
             onChange={handleSubmit}
             name="fileupload"
+            accept="image/*"
             style={{ opacity: "0", position: "absolute" }}
             id="hidden"
             type="file"
@@ -53,8 +63,13 @@ export const ImageUploadForm = ({ current, setImgSrc }: Curr) => {
           type="submit"
           id="submit"
           name="fileupload"
+          onSubmit={() => window.location.reload()}
         />
-        <MenuList sx={{ padding: "0.5em" }} onClick={handleDelete}>
+        <MenuList
+          className="img-form-menu"
+          sx={{ padding: "0.5em" }}
+          onClick={handleDelete}
+        >
           Remove
         </MenuList>
       </form>
