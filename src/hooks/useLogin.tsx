5132fa2d99f6169d23b59eaf43786dc6bb1e2d6f
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-08-12 21:55:37 +0200

    fixed login

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -9,14 +9,17 @@ export const useLogin = () => {
   const login = async (username: string, password: string) => {
     setIsLoading(true);
     setError(null);
-    const response = await fetch(`http://localhost:5000/user/login/`, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    const response = await fetch(
+      `https://media-log.herokuapp.com/user/login/`,
+      {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      }
+    );
     const json = await response.json();
     if (!json.error) {
       localStorage.setItem("user", JSON.stringify(json));
