d0159ad11e0aba5da121e4c5ae75c46ce05c4a5c
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-11-07 16:05:16 +0100

    hosting

--- src/features/nav/hooks/usePopper.tsx
+++
@@ -1,8 +0,0 @@
-import { useRef, useState } from "react";
-
-export const usePopper = () => {
-  const anchorRef = useRef<HTMLButtonElement>(null);
-  const [open, setOpen] = useState(false);
-
-  return { anchorRef, open, setOpen };
-};
